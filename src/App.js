86ab0800bf4c008166de5fe52be28c76504e747e
Matthew Seidel <mseidel819@gmail.com>
2022-05-31 08:06:32 -0500

    initial commit. boilerplate.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { StyledContainer } from "./App.styles.jsx";
+import { Container, Grid } from "@mui/material";
+import { ReactComponent as LogoTitle } from "./images/logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledContainer maxWidth="lg">
+      <Grid container>
+        <Grid item container xs={12} justifyContent="center">
+          <LogoTitle />
+        </Grid>
+        <Grid container item></Grid>
+      </Grid>
+    </StyledContainer>
   );
 }
 
---
+++ src/App.styles.jsx
@@ -0,0 +1,7 @@
+import { styled, Container } from "@mui/material";
+
+export const StyledContainer = styled(Container)({
+  display: "flex",
+  alignItems: "center",
+  height: "100vh",
+});
